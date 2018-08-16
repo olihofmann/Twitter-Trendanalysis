8302d6c60a4045c446cf480f1ec31d88163c8e0a
Oliver Hofmann <oh@garaio.com>
2018-08-16 22:46:42 +0200

    Remove NLTK and add sparknlp

--- spark-streaming-kafka-cassandra/startup.sh
+++ spark-streaming-kafka-cassandra/startup.sh
@@ -5,13 +5,14 @@ export SPARK_HOME=/spark
 export HADOOP_HOME=/hadoop
 export PYSPARK_PYTHON=/usr/bin/python3
 export SPARK_DRIVER_PYTHON=/usr/bin/python3
+export PYTHONPATH="~/spark/jars/spark-nlp-assembly-1.6.1.jar:$PYTHONPATH"
 
 echo "Submit application ${SPARK_APPLICATION_PYTHON_LOCATION} to Spark master ${SPARK_MASTER_URL}"
 echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
 
 spark/bin/spark-submit \
     --master ${SPARK_MASTER_URL} \
-    --jars spark/jars/spark-sql-kafka-0-10_2.11-2.3.1.jar,spark/jars/kafka-clients-0.10.0.0.jar \
+    --jars spark/jars/spark-sql-kafka-0-10_2.11-2.3.1.jar,spark/jars/kafka-clients-0.10.0.0.jar,spark/jars/spark-nlp-assembly-1.6.1.jar \
     --num-executors 1 \
     --driver-memory 2g \
     --executor-memory 1g \
--- spark-streaming-kafka-cassandra/twitter_kafka_consumer.py
+++ spark-streaming-kafka-cassandra/twitter_kafka_consumer.py
@@ -1,19 +1,25 @@
 import sys
+import os
+import glob
 
+from pyspark import SparkContext, SparkFiles
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
-import nltk
+sys.path.extend(glob.glob(os.path.join(os.path.expanduser("~"), "spark/jars/*.jar")))
+from sparknlp.base import *
+from sparknlp.annotator import *
 
 schema = StructType([                                                                                          
     StructField("text", StringType(), True) 
 ])
 
 def extract_noun(text):
-    is_noun = lambda pos: pos[:2] == "NN"
-    tokens = nltk.word_tokenize(text)
-    return [word for (word, pos) in nltk.pos_tag(tokens) if is_noun(pos)]
+    return "Ice"
+    # is_noun = lambda pos: pos[:2] == "NN"
+    # tokens = nltk.word_tokenize(text)
+    # return [word for (word, pos) in nltk.pos_tag(tokens) if is_noun(pos)]
 
 # Create the Spark Session and the Application
 spark_session = SparkSession.builder.appName("TwitterTrendAnalyses").getOrCreate()
@@ -26,9 +32,16 @@ data_stream_transformed = twitter_data_stream.withWatermark("timestamp", "1 day"
 data_stream_string = data_stream_transformed.selectExpr("CAST(value AS STRING) as json")
 tweets_table = data_stream_string.select(from_json(col("json"), schema).alias("tweet"))
 
+# Get the tweet data
+tweet_text_table = tweets_table.select("tweet.text")
+
+# udf = USER DEFINED FUNCTION
+text_to_pos = udf(extract_noun, StringType())
+
 # Get the Text from the Tweet Table
-pos_tags_table = tweets_table.selectExpr(extract_verbs("tweet.text")).alias("pos_tags")
+noun_table = tweet_text_table.withColumn("nouns", text_to_pos("text"))
+#noun_table = tweets_table.selectExpr(text_to_pos("tweet.text")).alias("pos_tags")
 
 #new_dataFrame = tweet_text_table.withColumn("nlp_text", "tweet_text")
-query = pos_tags_table.writeStream.trigger(processingTime='10 seconds').format("console").start()
+query = noun_table.writeStream.trigger(processingTime='10 seconds').format("console").start()
 query.awaitTermination()
