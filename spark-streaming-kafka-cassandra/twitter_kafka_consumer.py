88a3a7e6ca816d014ee6dab3436f915087019743
Oliver Hofmann <oh@garaio.com>
2018-08-29 21:57:49 +0200

    Finish Cassandra implementation

--- spark-streaming-kafka-cassandra/twitter_kafka_consumer.py
+++ spark-streaming-kafka-cassandra/twitter_kafka_consumer.py
@@ -18,7 +18,9 @@ from cassandra.query import SimpleStatement
 
 KEYSPACE = "tweet_keyspace"
 schema = StructType([                                                                                          
-    StructField("text", StringType(), True)
+    StructField("text", StringType(), True),
+    StructField("retweet_count", IntegerType(), True),
+    StructField("favorite_count", IntegerType(), True)
 ])
 
 def createKeySpace():
@@ -48,6 +50,18 @@ def createKeySpace():
         print(e)
         pass
 
+def updateData(noun, date, count, retweets, likes):
+    print("Start persist")
+    cluster = Cluster(["cassandra"])
+    session = cluster.connect()
+
+    session.set_keyspace(KEYSPACE)
+    
+    prepared = session.prepare("""UPDATE tweets SET count=count + ?, retweets=retweets + ?, likes=likes + ?
+    WHERE noun=? AND timestamp=?""")
+    
+    session.execute(prepared, [count, retweets, likes, noun, date])
+
 def getSqlContextInstance(sparkContext):
     if ("sqlContextSingletonInstance" not in globals()):
         globals()["sqlContextSingletonInstance"] = SQLContext(sparkContext)
@@ -55,7 +69,9 @@ def getSqlContextInstance(sparkContext):
 
 def extract_noun(text):
     is_noun = lambda pos: pos[:2] == "NN" 
-    tokens = nltk.word_tokenize(text) 
+
+    tweet_tokenizer = nltk.tokenize.TweetTokenizer()
+    tokens = tweet_tokenizer.tokenize(text)
     return [word for (word, pos) in nltk.pos_tag(tokens) if is_noun(pos)] 
 
 def processTweets(time, rdd):
@@ -67,42 +83,38 @@ def processTweets(time, rdd):
         tweet_dataframe = sql_context.read.schema(schema).json(rdd)
 
         # Extract the relevant properties
-        extract_dataframe = tweet_dataframe.select("text")
+        extract_dataframe = tweet_dataframe.select("text", "retweet_count", "favorite_count")
 
         # Define the user defined function
         text_to_pos = udf(extract_noun, ArrayType(StringType()))
 
-        # Extract the nouns from the text
+        # Extract the data from the text
         text_noun_dataframe = extract_dataframe.withColumn("nouns", text_to_pos("text"))
 
         # Preparing the persist dataset
-        persist_dataframe = text_noun_dataframe.select(explode("nouns"))
+        persist_dataframe = text_noun_dataframe.select(explode("nouns"), "retweet_count", "favorite_count")
         persist_dataframe.show()
 
-        # rdd = rdd.context.parallelize([{
-	    #     "key": t[0],
-	    #     "timestamp": datetime.datetime.now().date(),
-	    #     "count": 1,
-	    #     "retweets": 10,
-	    #     "likes": 0
-        # } for t in persist_dataframe.collect()])
+        # Collect and persist to cassandra
+        for p in persist_dataframe.collect():
+            print("Persist noun: " + p[0] + " Date: " + datetime.datetime.now().date().isoformat())
+            updateData(p[0], datetime.datetime.now().date().isoformat(), 1, p[1], p[2])
 
-        # rdd.saveToCassandra(
-	    #     "nouns",
-	    #     "tweets")
-    except:
-        print("Error process tweet")
+    except Exception as e:
+        print("Error process tweet: " + e)
         pass
 
+# Create Spark Context
 spark_context = SparkContext(appName="TwitterTrendAnalyses") 
 stream_context = StreamingContext(spark_context, 10) 
  
+# Connect to Kafka
 kafka_stream = KafkaUtils.createDirectStream(stream_context, ["Tweets"], {"metadata.broker.list": "192.168.1.106:9092"}) 
 kafka_stream.map(lambda rawTweet: rawTweet[1]).foreachRDD(processTweets)
-# parsed_stream = kafka_stream.map(lambda rawTweet: json.loads(rawTweet[1]))
-# parsed_stream.pprint()
-#parsed_stream.foreachRDD(processTweets)
 
+# Create Cassandra Keyspace
 createKeySpace()
+
+# Start Streaming
 stream_context.start() 
 stream_context.awaitTermination()
